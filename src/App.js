702018b9e5a25c9bd731a60b22cdea3e056cb306
Ransford Walker <ransfordw@gmail.com>
2019-09-30 15:24:25 -0400

    added some dynamic content examples for the person component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hi, I am an React App</h1>
+        <h1> Hi, I am a React App</h1>
         <p>This is really working!</p> 
-        <Person/> 
+        <Person name="John" age="28"/> 
+        <Person name="Jane" age="22"/> 
+        <Person name="Renault" age="53"/> 
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const person = () =>{
-    return <p>I'm a Person!</p>
+const person = (props) =>{
+    //return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+return <p>Hello! My name is {props.name} and I am {props.age}</p>
 };
 
 export default person;
