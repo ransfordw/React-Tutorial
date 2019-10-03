6d394df6d361d7ec11b213f9394f76de86fe96f5
Ransford Walker <ransfordw@gmail.com>
2019-10-03 08:59:45 -0400

    Duplicated some person components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'John', age: 28 },
+      { name: 'Jane', age: 22 },
+      { name: 'Renault', age: 53 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1> Hi, I am a React App</h1>
-        <p>This is really working!</p> 
-        <Person name="John" age="28"/> 
-        <Person name="Jane" age="22"/> 
-        <Person name="Renault" age="53"/> 
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Fishing</Person>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const person = (props) =>{
+const person = (props) => {
     //return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
-return <p>Hello! My name is {props.name} and I am {props.age}</p>
+    return (
+        <div>
+            <p>Hello! My name is {props.name} and I am {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
